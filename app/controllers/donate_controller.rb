0afca1d8b8ba135e4a500f17202b78043f7e429e
Karim Timer <abdikarimtimer@gmail.com>
2020-10-25 14:37:13 +0000

    basic pop card for a foodbank

--- app/controllers/donate_controller.rb
+++ app/controllers/donate_controller.rb
@@ -1,10 +1,9 @@
 class DonateController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
   def index
+    # @initial_foodbanks = Search::Foodbank.new("").initial_find
+    # gon.your_array = @initial_foodbanks
+    return unless params[:search]
+    @foodbank = Search::Foodbank.new(params[:search]).find
+    gon.your_array = @foodbank
   end
 end
--- app/controllers/foodbank_controller.rb
+++ app/controllers/foodbank_controller.rb
@@ -1,14 +1,6 @@
 require 'gon'
 class FoodbankController < ApplicationController
 
-  # def search
-  #   if params[:search].blank?  
-  #     redirect_to('foodbank/index', alert: "Empty field!") and return  
-  #   else  
-
-  #   end  
-  # end
-
   def index
     @initial_foodbanks = Search::Foodbank.new("").initial_find
     gon.your_array = @initial_foodbanks

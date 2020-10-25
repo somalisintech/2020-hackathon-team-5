5e08939ed26c1c887173a2cb6f55f5d2322d5c4a
Karim Timer <abdikarimtimer@gmail.com>
2020-10-25 17:04:14 +0000

    fix donate empty search

--- app/controllers/donate_controller.rb
+++ app/controllers/donate_controller.rb
@@ -2,8 +2,9 @@ class DonateController < ApplicationController
   def index
     # @initial_foodbanks = Search::Foodbank.new("").initial_find
     # gon.your_array = @initial_foodbanks
-    return unless params[:search]
-    @foodbank = Search::Foodbank.new(params[:search]).find
+    return if params[:donateSearch].to_s.strip.empty?
+
+    @foodbank = Search::Foodbank.new(params[:donateSearch]).find
     @donations = @foodbank.pluck(:needs)
     gon.your_array = @donations
   end
